bdc744bc7a179f6a311439811ca8a298024880e7
suxin1 <suxin1@live.com>
2017-08-07 08:27:44 +0800

    completed ps10

--- Lecture_22/ps10/ps10.py
+++ Lecture_22/ps10/ps10.py
@@ -6,45 +6,63 @@
 #Code shared across examples
 import pylab, random, string, copy, math
 
+
 class Point(object):
     def __init__(self, name, originalAttrs, normalizedAttrs = None):
         """normalizedAttrs and originalAttrs are both arrays"""
         self.name = name
         self.unNormalized = originalAttrs
         self.attrs = normalizedAttrs
+
     def dimensionality(self):
         return len(self.attrs)
+
     def getAttrs(self):
         return self.attrs
+
     def getOriginalAttrs(self):
         return self.unNormalized
+
     def distance(self, other):
         #Euclidean distance metric
         difference = self.attrs - other.attrs
         return sum(difference * difference) ** 0.5
+
     def getName(self):
         return self.name
+
     def toStr(self):
         return self.name + str(self.attrs)
+
     def __str__(self):
         return self.name
 
+
 class County(Point):
     weights = pylab.array([1.0] * 14)
-    
+
+    def __init__(self, name, originalAttrs, normalizedAttrs, weights):
+        Point.__init__(self, name, originalAttrs, normalizedAttrs)
+
+        if weights != None:
+            County.weights = weights
+
     # Override Point.distance to use County.weights to decide the
     # significance of each dimension
     def distance(self, other):
         difference = self.getAttrs() - other.getAttrs()
         return sum(County.weights * difference * difference) ** 0.5
-    
+
+
 class Cluster(object):
     def __init__(self, points, pointType):
         self.points = points
         self.pointType = pointType
         self.centroid = self.computeCentroid()
+
     def getCentroid(self):
         return self.centroid
+
     def computeCentroid(self):
         dim = self.points[0].dimensionality()
         totVals = pylab.array([0.0]*dim)
@@ -52,8 +70,9 @@ class Cluster(object):
             totVals += p.getAttrs()
         meanPoint = self.pointType('mean',
                                    totVals/float(len(self.points)),
-                                   totVals/float(len(self.points)))
+                                   totVals/float(len(self.points)), None)
         return meanPoint
+
     def update(self, points):
         oldCentroid = self.centroid
         self.points = points
@@ -62,34 +81,37 @@ class Cluster(object):
             return oldCentroid.distance(self.centroid)
         else:
             return 0.0
+
     def getNameSet(self):
         nameset = [p.getName() for p in self.points]
         return nameset
 
     def getPoints(self):
         return self.points
+
     def contains(self, name):
         for p in self.points:
             if p.getName() == name:
                 return True
         return False
+
     def toStr(self):
         result = ''
         for p in self.points:
             result = result + p.toStr() + ', '
         return result[:-2]
+
     def __str__(self):
         result = ''
         for p in self.points:
             result = result + str(p) + ', '
         return result[:-2]
-        
 
-    
+
 def kmeans(points, k, cutoff, pointType, minIters = 3, maxIters = 100, toPrint = False):
     """ Returns (Cluster list, max dist of any point to its cluster) """
     #Uses random initial centroids
-    initialCentroids = random.sample(points,k)
+    initialCentroids = random.sample(points, k)
     clusters = []
     for p in initialCentroids:
         clusters.append(Cluster([p], pointType))
@@ -161,8 +183,8 @@ def readCountyData(fName, numEntries = 14):
         nameList.append(name)
     return nameList, dataList, maxVals
 
-    
-def buildCountyPoints(fName):
+
+def buildCountyPoints(fName, weights):
     """
     Given an input filename, reads County values from the file and returns
     them all in a list.
@@ -172,7 +194,7 @@ def buildCountyPoints(fName):
     for i in range(len(nameList)):
         originalAttrs = pylab.array(featureList[i])
         normalizedAttrs = originalAttrs/pylab.array(maxVals)  # Normalized feature's value is between 0 and 1
-        points.append(County(nameList[i], originalAttrs, normalizedAttrs))
+        points.append(County(nameList[i], originalAttrs, normalizedAttrs, weights))
     return points
 
 def randomPartition(l, p):
@@ -180,10 +202,10 @@ def randomPartition(l, p):
     Splits the input list into two partitions, where each element of l is
     in the first partition with probability p and the second one with
     probability (1.0 - p).
-    
+
     l: The list to split
     p: The probability that an element of l will be in the first partition
-    
+
     Returns: a tuple of lists, containing the elements of the first and
     second partitions.
     """
@@ -200,9 +222,9 @@ def getAveIncome(cluster):
     """
     Given a Cluster object, finds the average income field over the members
     of that cluster.
-    
+
     cluster: the Cluster object to check
-    
+
     Returns: a float representing the computed average income value
     """
     tot = 0.0
@@ -213,7 +235,7 @@ def getAveIncome(cluster):
     return float(tot) / len(cluster.getPoints())
 
 
-def test(points, k = 200, cutoff = 0.1):
+def test(points, k=200, cutoff=0.1):
     """
     A sample function to show you how to do a simple kmeans run and graph
     the results.
@@ -231,15 +253,11 @@ def test(points, k = 200, cutoff = 0.1):
     pylab.ylabel('Number of Clusters')
     pylab.show()
 
-        
-points = buildCountyPoints('counties.txt')
-# random.seed(123)
-testPoints = random.sample(points, len(points))
-# test(testPoints)
 
+# --------------------------------------------------------
+#                          Problem 1
+# --------------------------------------------------------
 
-    
-    
 def graphRemovedErr(points, kvals = [25, 50, 75, 100, 125, 150], cutoff = 0.1):
     """
     Should produce graphs of the error in training and holdout point sets, and
@@ -279,7 +297,6 @@ def graphRemovedErr(points, kvals = [25, 50, 75, 100, 125, 150], cutoff = 0.1):
         trainingSetE.append(errorT)
 
     hrRatio = [holdoutSetE[i]/trainingSetE[i] for i in range(len(kvals))]
-
     pylab.figure(1)
     pylab.plot(kvals, holdoutSetE, '--r', label="hold out set error")
     pylab.plot(kvals, trainingSetE, '-g', label="training set error")
@@ -294,6 +311,11 @@ def graphRemovedErr(points, kvals = [25, 50, 75, 100, 125, 150], cutoff = 0.1):
     pylab.show()
 
 
+# --------------------------------------------------------
+#                          Problem 2
+# --------------------------------------------------------
+
+
 # graphRemovedErr(testPoints)
 
 def observeOneCounty(points):
@@ -303,6 +325,10 @@ def observeOneCounty(points):
     # Create file
     file = open("result", "w")
     file.write("Clusters which includes " + county + " in 10 time clustering\n")
+
+    clusterset = []
+    clusterOblist = []
+
     for i in range(10):
         clusters, maxDis = kmeans(points, 50, 0.1, County)
         for i in range(len(clusters)):
@@ -312,15 +338,58 @@ def observeOneCounty(points):
 
             for p in c.getPoints():
                 if p.getName() == county:
-                    file.write("-----------clusters-----------\n" + str(c.getNameSet()))
+                    clusterset.append(c.getNameSet())
+                    clusterOblist.append(c)
                     isFound = True
                     break
             if isFound:
                 break
-    file.close()
 
-observeOneCounty(testPoints)
+    return clusterset, clusterOblist
 
+
+def printOut(cluster):
+    points = cluster.getPoints()
+    for p in points:
+        values = [round(attr, 3) for attr in p.getAttrs()]
+        print (p.getName(), values)
+
+
+def analyzeCluster(clusterset):
+    numList = []
+    rotiolist = []
+
+    for i in range(1, len(clusterset)):
+        num = 0
+        for p in clusterset[i]:
+            if p in clusterset[0]:
+                num += 1
+            print ("cluster {} point {} {} {}".format(i, p, p in clusterset[0], num))
+        rotiolist.append(num/len(clusterset[i]))
+        numList.append(len(clusterset[i]))
+    print clusterset
+
+
+    pylab.figure(1)
+    pylab.subplot(211)
+    pylab.plot(rotiolist, 'bo')
+    pylab.ylabel("similarity")
+    pylab.xlabel("number of trials")
+
+    pylab.subplot(212)
+    pylab.plot(numList, 'ro')
+    pylab.ylabel("total number of points in a cluster")
+    pylab.xlabel("number of trials")
+    pylab.show()
+
+
+# clusterset, clusters = observeOneCounty(testPoints)
+# analyzeCluster(clusterset)
+
+
+# --------------------------------------------------------
+#                          Problem 3
+# --------------------------------------------------------
 
 def graphPredictionErr(points, dimension, kvals = [25, 50, 75, 100, 125, 150], cutoff = 0.1):
     """
@@ -328,6 +397,85 @@ def graphPredictionErr(points, dimension, kvals = [25, 50, 75, 100, 125, 150], c
     appropriate values of k and graph the error in the resulting predictions,
     as described in Problem 3.
     """
+    averagePovertys = []
+    averageDiff = []
 
-	# Your Code Here
-    
+    l1, l2 = randomPartition(points, 0.8)
+
+    for k in kvals:
+        clusters, maxDis = kmeans(l1, k, cutoff, County)
+
+        # Total Error for each k where TE = Sum(distance of point to the centroid of its encapsulating cluster)
+        for c in clusters:
+            points = c.getPoints()
+            if len(points) < 1:
+                averagePovertys.append(0)
+                continue
+
+            povertyValues = [p.getOriginalAttrs()[dimension] for p in points]
+
+            try:
+                averagePovertys.append(sum(povertyValues)/len(points))
+            except ZeroDivisionError:
+                continue
+
+        # For each point in l2 list find its distance to its nearest cluster
+        squaredDiff = []
+        for p in l2:
+            # Find the smallest square distance from point to all the clusters
+            smallestDis = p.distance(clusters[0].getCentroid())
+            nearestCluster = clusters[0]
+            nearestClusterIndex = 0
+
+            for i in range(len(clusters)):
+                currentDis = p.distance(clusters[i].getCentroid())
+                if currentDis < smallestDis:
+                    smallestDis = currentDis
+                    nearestCluster = c
+                    nearestClusterIndex = i
+
+            squaredDiff.append((p.getOriginalAttrs()[dimension] - averagePovertys[nearestClusterIndex]) ** 2)
+
+        averageDiff.append(sum([sd for sd in squaredDiff]) / len(squaredDiff))
+
+
+
+    # pylab.figure(1)
+    # pylab.plot(kvals, averageDiff, '-g', label="poverty mean difference")
+    # pylab.xlabel("K (Number of clusters)")
+    # pylab.ylabel("poverty mean difference")
+    # pylab.show()
+
+    return averageDiff
+
+
+def findBest():
+    kvals = [25, 50, 75, 100, 125, 150]
+    weightsSet = []
+    for i in range(10):
+        weights = pylab.array([random.choice([0, 1]) for i in range(14)])
+        weights[2] = 0
+        weightsSet.append(weights)
+
+    averageDiff = None
+    bestWeight = []
+    for weights in weightsSet:
+        points = buildCountyPoints('counties.txt', weights)
+        averageDiffTemp = graphPredictionErr(points, 2)
+
+        if averageDiff == None:
+            averageDiff = averageDiffTemp
+            bestWeight = weights
+
+        elif max(averageDiffTemp) < max(averageDiff):
+            averageDiff = averageDiffTemp
+            bestWeight = weights
+
+    print bestWeight
+    pylab.figure(1)
+    pylab.plot(kvals, averageDiff, '-g', label="poverty mean difference")
+    pylab.xlabel("K (Number of clusters)")
+    pylab.ylabel("poverty mean difference")
+    pylab.show()
+
+findBest()
---
+++ Lecture_22/ps10/ps10writeup.md
@@ -0,0 +1,38 @@
+ ## Problem 1: Removed Error
+
+#### Defination:
+
+* **Training**: Use a set of data to training the Algorithem to get cluster as we wanted.
+
+
+* **Training set**: A set containing a number of points randomly choosen from all points.
+* **Holdout set**: All points subtract **Training set**.
+
+
+* **Error**: Squared Distance between points from cluster and centroid of cluster.
+
+![some picture](./ps10_Figure_1.png)
+
+Some explanation
+
+![some text](./ps10_Figure_2.png)
+
+Some Explanation
+
+## Problem 3: Prediction error
+
+#### Definition 
+
+* **Error**: Differences between average of poverty of clusters and average poverty of all points to it's nearest cluster.
+
+
+
+Figure 1: only poverty set to be 0 to make sure it didn't interfere the prediction
+
+![some text](predictionError.png)
+
+Figure 2: I randomly set propertys other then poverty to 0 or 1 while keeping poverty 0. after 10 times runing the clustering algorithem, I found the following may be best in this test.
+
+**Weight applied**: [1 0 0 0 1 0 1 0 0 1 1 0 0 1] 
+
+I lost the plot for this test.
